97d94142db5bb081646cfdb468a9a3adcd00fe82
Killian D.B. <115996000+KillianDB@users.noreply.github.com>
2025-08-07 01:18:51 +0000

    feat: importing submodules

--- ESTUDANTES/src/App.java
+++ ESTUDANTES/src/App.java
@@ -1,5 +1,8 @@
+import colecao.ColecaoGenerico;
+import estudantes-codigo.Estudante;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        var col = ColecaoGenerico();
     }
 }
